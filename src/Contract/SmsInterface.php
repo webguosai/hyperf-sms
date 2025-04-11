df9fc23e3c3978b9883d2b1cf72f6c6c133bbadc
guosai <501807312@qq.com>
2025-04-11 13:55:26 +0800

    1

--- src/Contract/SmsInterface.php
+++ src/Contract/SmsInterface.php
@@ -18,6 +18,13 @@ interface SmsInterface
     public function send(string $mobile, array $message): void;
 
     /**
+     * 格式化message
+     * @param array $message
+     * @return MessageInterface
+     */
+    public function formatMessage(array $message): MessageInterface;
+
+    /**
      * 获取驱动
      * @return string
      */
